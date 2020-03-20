2d8afaf9a38e989cb980a34e78eb9119813f2c0c
piyushmaheswari <piyush.ext@grofers.com>
2020-03-20 23:43:53 +0530

    More Changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to srt working on </Text>
+      <Text>VPN App </Text>
     </View>
   );
 }
